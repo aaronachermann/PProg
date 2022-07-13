efb6b964fc14679d9b060945be5bec8b3276d929
aaronachermann <88669642+aaronachermann@users.noreply.github.com>
2022-07-13 17:45:21 +0200

    Update not_Reusable_Barrier.java

--- not_Reusable_Barrier.java
+++ not_Reusable_Barrier.java
@@ -2,11 +2,11 @@ public class MyBarrier {
   private int n;//number of threads
 
   MyBarrier (int n){
-    this.n=n;
+    this.n = n;
   }
   synchronized void await() throws InterruptedException{
     n--;
-    if (n==0) notifyAll():
-    else while(n>0) {wait();}
+    if (n == 0) notifyAll():
+    else while(n > 0) {wait();}
   }
 }
