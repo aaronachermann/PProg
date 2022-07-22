7f33657757648dccc5bd217eb9fc762667746a3d
aaronachermann <88669642+aaronachermann@users.noreply.github.com>
2022-07-22 15:41:59 +0200

    Update Reusable_Barrier.java

--- Reusable_Barrier.java
+++ Reusable_Barrier.java
@@ -2,20 +2,20 @@
 class MyCyclicBarrier {
 	private final Runnable barrierAction;
 	private final int n; // total number ofthreads
-	private int reachThreads; // number of threads stil needed to reach the barrier
+	private int awaitThreads; // number of threads stil needed to reach the barrier
 
 	public MyCyclicBarrier(int n, Runnable barrierAction) {
 		this.n = n;
-		this.reachThreads = n;
+		this.awaitThreads = n;
 		this.barrierAction = barrierAction;
 	}
 
 	public synchronized void await() throws InterruptedException {
-		this.reachThreads--;
-		if (this.reachThreads > 0) {
+		this.awaitThreads--;
+		if (this.awaitThreads > 0) {
 			this.wait();
 		} else {
-			this.reachThreads = this.n;
+			this.awaitThreads = this.n;
 			this.barrierAction.run();
 			this.notifyAll();
 		}
